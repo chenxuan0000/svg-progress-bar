84a7c70c51da1b69ad8cb1a720fbc8af1182cbb7
chenxuan <chenxuan@kongzhongjr.com>
2018-01-10 11:56:19 +0800

    :white_check_mark: add a en/chinese Readme

--- examples-src/App.vue
+++ examples-src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <div class="fixed top-0 w-100 z-1 flex-none flex flex-row items-center pv3 ph4 bg-blue white">
-            <div class="flex-none"><a href="https://github.com/chenxuan0000/vue-progress-bar" rel="noopener"
+            <div class="flex-none"><a href="https://github.com/chenxuan0000/svg-progress-bar" rel="noopener"
                                       target="_blank" title="View on Github">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="white" width="32" height="32" viewBox="0 0 16 16">
                     <path d="M8 .198c-4.418 0-8 3.582-8 8 0 3.535 2.292 6.533 5.47 7.59.4.075.548-.173.548-.384 0-.19-.008-.82-.01-1.49-2.227.485-2.696-.943-2.696-.943-.364-.924-.888-1.17-.888-1.17-.726-.497.055-.486.055-.486.802.056 1.225.824 1.225.824.714 1.223 1.872.87 2.328.665.072-.517.28-.87.508-1.07-1.776-.202-3.644-.888-3.644-3.954 0-.874.313-1.588.824-2.148-.083-.202-.357-1.015.077-2.117 0 0 .672-.215 2.2.82.64-.177 1.323-.266 2.003-.27.68.004 1.365.093 2.004.27 1.527-1.035 2.198-.82 2.198-.82.435 1.102.162 1.916.08 2.117.512.56.822 1.274.822 2.147 0 3.072-1.872 3.748-3.653 3.946.288.248.544.735.544 1.48 0 1.07-.01 1.933-.01 2.196 0 .213.145.462.55.384 3.178-1.06 5.467-4.057 5.467-7.59 0-4.418-3.58-8-8-8z"></path>
@@ -9,16 +9,16 @@
             </a>
             </div>
             <div class="flex-none" style="margin-left: 10px;"><a
-                    href="https://github.com/chenxuan0000/vue-progress-bar"
-                    class="white no-underline underline-hover">vue-progress-bar</a></div>
+                    href="https://github.com/chenxuan0000/svg-progress-bar"
+                    class="white no-underline underline-hover">svg-progress-bar</a></div>
         </div>
         <div class="flex-fill" style="padding-bottom: 10rem">
             <div class="mb5 center" style="max-width: 57rem">
-                <div class="pt5 nb5" id="default-spinners">
+                <div class="pt5 nb5">
                     <a href="#default-spinners" :class="header_cls">
-                        <h2 class="ma0 pb2 f2 fw4 lh-1">Default vue-progress-bar</h2>
+                        <h2 class="ma0 pb2 f2 fw4 lh-1">Default svg-progress-bar</h2>
                         <p class="ma0 lh-copy f6">
-                            vue-progress-bar can be used with no configuration or very little configuration.</p>
+                            svg-progress-bar can be used with no configuration or very little configuration.</p>
                     </a>
                     <div :class="box_cls" :style="box_style">
                         <div :class="label_cls">Default circle</div>
