db43473fc1c18037c1135e9ed9df1f734f6d1112
cdswyda <cdswyda@hotmail.com>
2018-01-19 08:26:51 +0800

    unnecessary use in global

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import vueProgress from './components/progress-bar.vue'
 
-// expose component to global scope
-if (typeof window !== 'undefined' && window.Vue) {
-  Vue.component('svg-progress-bar', vueProgress)
-}
-
 export { vueProgress }
 
 export default vueProgress
